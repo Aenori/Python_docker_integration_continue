4cfa33651efd75366a3c925f3c50135b74fb372c
Nicolas <Nicolas Rousset>
2021-04-06 21:35:27 +0200

    Testing with convert

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -l
+set -eo
 
-echo $(convert -resize 25% American-shorthair-cat-scaled.jpg American-shorthair-cat-small.jpg)
-echo $(file American*)
+convert -resize 25% American-shorthair-cat-scaled.jpg American-shorthair-cat-small.jpg
+
+file American-shorthair-cat-scaled.jpg American-shorthair-cat-small.jpg
+
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
